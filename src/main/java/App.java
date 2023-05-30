9d16eb8635973c43cbb758b6f46dc50fa7a6d53f
Érico Borgonove <borgonove@gmail.com>
2023-05-29 20:35:03 -0400

    enviando os arquivos do dia 29/05

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+
+import Views.MenuPrincipal;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author erico
+ */
+public class App {
+
+    public static void main(String[] args) {
+        MenuPrincipal mp = new MenuPrincipal();
+        mp.setVisible(true);
+    }
+}
